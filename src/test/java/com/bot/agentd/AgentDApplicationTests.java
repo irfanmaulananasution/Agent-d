29341cc1aa6dc0a1dbdb60c7dde352efaa608c89
raihan2604 <raihan2604@gmail.com>
2020-04-07 08:41:13 +0700

    initial commit

---
+++ src/test/java/com/bot/agentd/AgentDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.agentd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
