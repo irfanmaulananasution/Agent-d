d820c6f95d76b99ec29067f956ae471d2de484f6
raihan2604 <raihan2604@gmail.com>
2020-04-28 22:08:25 +0700

    [REFACTOR] move methods and improve overall coverage

--- src/main/java/com/bot/agentd/AgentDApplication.java
+++ src/main/java/com/bot/agentd/AgentDApplication.java
@@ -13,15 +13,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 @SpringBootApplication
 @LineMessageHandler
@@ -32,8 +26,7 @@ public class AgentDApplication extends SpringBootServletInitializer {
 
     static HashMap<String, UserAgentD> repo = new HashMap<>();
     String tidakDikenal = "Command tidak dikenal";
-    LogManager lgmngr = LogManager.getLogManager();
-    Logger log = lgmngr.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
@@ -53,7 +46,7 @@ public class AgentDApplication extends SpringBootServletInitializer {
             repo.put(userId,new UserAgentD(userId));
         }
         UserAgentD user = repo.get(userId);
-        String jawaban = periksaMessage(pesanSplit, user);
+        String jawaban = user.periksaMessage(pesanSplit);
 
         String replyToken = messageEvent.getReplyToken();
         replyText(replyToken, jawaban);
@@ -65,57 +58,6 @@ public class AgentDApplication extends SpringBootServletInitializer {
         repo.put(event.getSource().getSenderId(),user);
     }
 
-    private String periksaMessage(String[] pesanSplit, UserAgentD user){
-        String jawaban = "";
-        switch (pesanSplit[0].toLowerCase()){
-            case("tambah"):
-                switch (pesanSplit[1].toLowerCase()){
-                    case("tugas individu"):
-                        jawaban = tambahTugasIndividu(pesanSplit[2], pesanSplit[3], pesanSplit[4], user);
-                        break;
-                    default:
-                        jawaban = tidakDikenal;
-                }
-                break;
-            case("lihat"):
-                switch (pesanSplit[1].toLowerCase()){
-                    case ("tugas individu"):
-                        jawaban = lihatTugasIndividu(user);
-                        break;
-                    default:
-                        jawaban = tidakDikenal;
-                }
-                break;
-            default:
-                jawaban = tidakDikenal;
-        }
-
-        return jawaban;
-    }
-
-    private String tambahTugasIndividu(String nama, String deskripsi, String deadline, UserAgentD user) {
-        try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-            Date tanggal = dateFormat.parse(deadline);
-            TugasIndividu tugas = new TugasIndividu(nama, deskripsi, tanggal);
-            user.addTugasIndividu(tugas);
-            return nama + " berhasil ditambahkan sebagai tugas individu";
-        }catch (ParseException e){
-            log.log(Level.INFO, "Error while parsing the date");
-            return "Tanggal tidak dikenal";
-        }
-    }
-
-    private String lihatTugasIndividu(UserAgentD user){
-        String jawaban = "";
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        for(int i = 0;i<user.listTugasIndividu.size();i++){
-            jawaban+="nama tugas : "+user.getTugasIndividu().get(i).getName()+"\n";
-            jawaban+="deskripsi : "+user.getTugasIndividu().get(i).getDesc()+"\n";
-            jawaban+="deadline : "+dateFormat.format(user.getTugasIndividu().get(i).getDeadline())+"\n\n";
-        }
-        return jawaban;
-    }
 
 
     private void replyText(String replyToken, String jawaban){
@@ -125,7 +67,7 @@ public class AgentDApplication extends SpringBootServletInitializer {
                     .replyMessage(new ReplyMessage(replyToken, jawabanDalamBentukTextMessage))
                     .get();
         } catch (InterruptedException | ExecutionException e) {
-            log.log(Level.INFO, "Error while sending message");
+            UserAgentD.log.log(Level.INFO, "Error while sending message");
             Thread.currentThread().interrupt();
         }
     }
--- src/main/java/com/bot/agentd/UserAgentD.java
+++ src/main/java/com/bot/agentd/UserAgentD.java
@@ -1,10 +1,20 @@
 package com.bot.agentd;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class UserAgentD {
     String id;
     ArrayList<TugasIndividu> listTugasIndividu;
+    String tidakDikenal = "Command Tidak Dikenali";
+    static LogManager lgmngr = LogManager.getLogManager();
+    static Logger log = lgmngr.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
 
     UserAgentD(String userID){
         id = userID;
@@ -19,4 +29,55 @@ public class UserAgentD {
         return this.listTugasIndividu;
     }
 
+    public String periksaMessage(String[] pesanSplit){
+        String jawaban = "";
+        switch (pesanSplit[0].toLowerCase()){
+            case("tambah"):
+                switch (pesanSplit[1].toLowerCase()){
+                    case("tugas individu"):
+                        jawaban = this.tambahTugasIndividu(pesanSplit[2], pesanSplit[3], pesanSplit[4]);
+                        break;
+                    default:
+                        jawaban = tidakDikenal;
+                }
+                break;
+            case("lihat"):
+                switch (pesanSplit[1].toLowerCase()){
+                    case ("tugas individu"):
+                        jawaban = this.lihatTugasIndividu();
+                        break;
+                    default:
+                        jawaban = tidakDikenal;
+                }
+                break;
+            default:
+                jawaban = tidakDikenal;
+        }
+
+        return jawaban;
+    }
+
+    public String tambahTugasIndividu(String nama, String deskripsi, String deadline) {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            Date tanggal = dateFormat.parse(deadline);
+            TugasIndividu tugas = new TugasIndividu(nama, deskripsi, tanggal);
+            this.addTugasIndividu(tugas);
+            return nama + " berhasil ditambahkan sebagai tugas individu";
+        }catch (ParseException e){
+            log.log(Level.INFO, "Error while parsing the date");
+            return "Tanggal tidak dikenal";
+        }
+    }
+
+    public String lihatTugasIndividu(){
+        String jawaban = "";
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        for(int i = 0;i<this.listTugasIndividu.size();i++){
+            jawaban+="nama tugas : "+this.getTugasIndividu().get(i).getName()+"\n";
+            jawaban+="deskripsi : "+this.getTugasIndividu().get(i).getDesc()+"\n";
+            jawaban+="deadline : "+dateFormat.format(this.getTugasIndividu().get(i).getDeadline())+"\n\n";
+        }
+        return jawaban;
+    }
 }
