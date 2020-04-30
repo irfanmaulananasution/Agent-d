bc7c92bd6c98b1a298ffbc25a670280d2b8e28e9
danielmartinn <danielmartin.sibarani@gmail.com>
2020-04-30 12:37:24 +0700

    [GREEN] add test

---
+++ src/test/java/com/bot/agentd/JadwalTests.java
@@ -0,0 +1,82 @@
+package com.bot.agentd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JadwalTests {
+    private Jadwal jadwal;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @BeforeEach
+    public void setUp() {
+        try {
+            Date date = dateFormat.parse("01/01/2020");
+            Date startTime = timeFormat.parse("13:00:00");
+            Date endTime = timeFormat.parse("15:00:00");
+            jadwal = new Jadwal("Jadwal Baru", date, startTime, endTime);
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testMethodGetName() {
+        assertEquals("Jadwal Baru", jadwal.getName());
+    }
+
+    @Test
+    public void testMethodSetName() {
+        jadwal.setName("Jadwal Baru 2");
+        assertEquals("Jadwal Baru 2", jadwal.getName());
+    }
+
+    @Test
+    public void testMethodGetDate(){
+        Date date = jadwal.getDate();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String strDate = dateFormat.format(date);
+        assertEquals("01/01/2020", strDate);
+    }
+
+    @Test
+    public void testMethodGetTimeStart(){
+        Date timeStart = jadwal.getTimeStart();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        String strTime = timeFormat.format(timeStart);
+        assertEquals("13:00:00", strTime);
+    }
+
+
+
+    @Test
+    public void testMethodGetTimeEnd(){
+        Date timeEnd = jadwal.getTimeEnd();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        String strTime = timeFormat.format(timeEnd);
+        assertEquals("15:00:00", strTime);
+    }
+
+
+//    @Test
+//    public void testSetDate(){
+//    }
+
+//    @Test
+//    public void testMethodSetDate(){
+//    }
+
+//    @Test
+//    public void testMethodSetTimeEnd(){
+//    }
+
+//    @Test
+//    public void testMethodRemove(){
+//    }
+}
--- src/test/java/com/bot/agentd/UserAgentDTest.java
+++ src/test/java/com/bot/agentd/UserAgentDTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -44,6 +45,42 @@ public class UserAgentDTest {
             assertEquals("tugas 1",user.getTugasIndividu().get(0).getName());
             assertEquals("tugas 2",user.getTugasIndividu().get(1).getName());
         }catch (ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testMethodAddJadwal(){
+        try{
+            assertEquals(0, user.getTugasIndividu().size());
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+            Date date = dateFormat.parse("05/04/2020");
+            Date timeStart = timeFormat.parse("13:00:00");
+            Date timeEnd = timeFormat.parse("14:00:00");
+            Jadwal jadwal1 = new Jadwal ("Jadwal 1", date, timeStart, timeEnd);
+            user.addJadwal(jadwal1);
+            ArrayList<Jadwal> listJadwal = user.getListJadwal();
+            assertEquals(1, listJadwal.size());
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testMethodGetJadwal() {
+        try {
+            assertEquals(0, user.getTugasIndividu().size());
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+            Date date = dateFormat.parse("05/04/2020");
+            Date timeStart = timeFormat.parse("13:00:00");
+            Date timeEnd = timeFormat.parse("14:00:00");
+            Jadwal jadwal1 = new Jadwal("Jadwal 1", date, timeStart, timeEnd);
+            user.addJadwal(jadwal1);
+            ArrayList<Jadwal> listJadwal = user.getListJadwal();
+            assertEquals("Jadwal 1", listJadwal.get(0).getName());
+        } catch (ParseException e) {
             e.printStackTrace();
         }
     }
