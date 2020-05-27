f32e9bfc7790516335131d5435536068f147d265
raihan2604 <raihan2604@gmail.com>
2020-05-28 00:55:42 +0700

    [GREEN] add quotes feature functional

--- src/main/java/com/bot/agentd/service/AgentDServiceImpl.java
+++ src/main/java/com/bot/agentd/service/AgentDServiceImpl.java
@@ -4,6 +4,10 @@ import com.bot.agentd.core.UserAgentD;
 import com.bot.agentd.repository.UserAgentDRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Level;
+
+import static com.bot.agentd.core.UserAgentD.qRepo;
+
 @Service
 public class AgentDServiceImpl implements AgentDService {
     UserAgentDRepository repository;
@@ -26,47 +30,59 @@ public class AgentDServiceImpl implements AgentDService {
     }
 
     public String periksaMessage(String id, String[] pesanSplit){
-        UserAgentD user = this.findUserById(id);
-        String jawaban = "";
-        switch (pesanSplit[0].toLowerCase()){
-            case("tambah"):
-                switch (pesanSplit[1].toLowerCase()){
-                    case("tugas individu"):
-                        jawaban = user.tambahTugasIndividu(pesanSplit[2], pesanSplit[3], pesanSplit[4]);
-                        break;
-                    case("tugas kelompok"):
-                        jawaban = user.tambahTugasKelompok(pesanSplit[2], pesanSplit[3], pesanSplit[4]);
-                        break;
-                    case ("jadwal"):
-                        String name = pesanSplit[2];
-                        String day = pesanSplit[3];
-                        String startTime = pesanSplit[4];
-                        String endTime = pesanSplit[5];
-                        jawaban = user.tambahJadwal(name, day, startTime, endTime);
-                        break;
-                    default:
-                        jawaban = tidakDikenal;
-                }
-                break;
-            case("lihat"):
-                switch (pesanSplit[1].toLowerCase()){
-                    case ("tugas individu"):
-                        jawaban = user.lihatTugasIndividu();
-                        break;
-                    case ("tugas kelompok"):
-                        jawaban = user.lihatTugasKelompok();
-                        break;
-                    case ("jadwal"):
-                        jawaban = user.lihatJadwal();
-                        break;
-                    default:
-                        jawaban = tidakDikenal;
-                }
-                break;
-            default:
-                jawaban = tidakDikenal;
-        }
+        try {
+            UserAgentD user = this.findUserById(id);
+            String jawaban = "";
+            switch (pesanSplit[0].toLowerCase()) {
+                case ("quote"):
+                    if (!qRepo.isInitiated()) {
+                        qRepo.repoInitiation();
+                    }
+                    jawaban = qRepo.getQuote();
+                    break;
 
-        return jawaban;
+                case ("tambah"):
+                    switch (pesanSplit[1].toLowerCase()) {
+                        case ("tugas individu"):
+                            jawaban = user.tambahTugasIndividu(pesanSplit[2], pesanSplit[3], pesanSplit[4]);
+                            break;
+                        case ("tugas kelompok"):
+                            jawaban = user.tambahTugasKelompok(pesanSplit[2], pesanSplit[3], pesanSplit[4]);
+                            break;
+                        case ("jadwal"):
+                            String name = pesanSplit[2];
+                            String day = pesanSplit[3];
+                            String startTime = pesanSplit[4];
+                            String endTime = pesanSplit[5];
+                            jawaban = user.tambahJadwal(name, day, startTime, endTime);
+                            break;
+                        default:
+                            jawaban = tidakDikenal;
+                    }
+                    break;
+                case ("lihat"):
+                    switch (pesanSplit[1].toLowerCase()) {
+                        case ("tugas individu"):
+                            jawaban = user.lihatTugasIndividu();
+                            break;
+                        case ("tugas kelompok"):
+                            jawaban = user.lihatTugasKelompok();
+                            break;
+                        case ("jadwal"):
+                            jawaban = user.lihatJadwal();
+                            break;
+                        default:
+                            jawaban = tidakDikenal;
+                    }
+                    break;
+                default:
+                    jawaban = tidakDikenal;
+            }
+
+            return jawaban;
+        }catch (Exception e){
+            UserAgentD.log.log(Level.INFO, "Error Happens in quote initiating");
+            return "Error Occured while initiating Quotes.";
+        }
     }
 }
