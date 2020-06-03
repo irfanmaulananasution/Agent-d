3e4c4d9cfe49909bcc9afcbd9f2e3039b33535da
raihan2604 <raihan2604@gmail.com>
2020-06-03 15:16:32 +0700

    test tugas individu using JPA

--- src/main/java/com/bot/agentd/core/TugasKelompok.java
+++ src/main/java/com/bot/agentd/core/TugasKelompok.java
@@ -3,75 +3,75 @@ package com.bot.agentd.core;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class TugasKelompok implements Tugas {
-    String name;
-    String desc;
-    Date deadline;
-    ArrayList<UserAgentD> anggota;
-
-    TugasKelompok(String name,String desc, Date deadline, UserAgentD user){
-        this.name = name;
-        this.desc = desc;
-        this.deadline = deadline;
-        this.anggota = new ArrayList<>();
-        anggota.add(user);
-    }
-
-    public void addAnggota(UserAgentD anggota){
-        if(!isAnggotaExist(anggota)) {
-            this.anggota.add(anggota);
-            anggota.addTugasKelompok(this);
-        }
-    }
-
-    public void removeAnggota(UserAgentD anggota){
-        if(isAnggotaExist(anggota)){
-            this.anggota.remove(anggota);
-            anggota.removeTugasKelompok(this);
-        }
-    }
-
-    public boolean isAnggotaExist(UserAgentD anggota){
-        return this.anggota.contains(anggota);
-    }
-
-    public void remindDeadline(){
-        for(UserAgentD ang : anggota){
-            ang.remindTugasKelompok(this);
-        }
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String getDesc() {
-        return desc;
-    }
-
-    @Override
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    @Override
-    public Date getDeadline() {
-        return deadline;
-    }
-
-    @Override
-    public void setDeadline(Date deadline) {
-        this.deadline = deadline;
-    }
-
-    public ArrayList<UserAgentD> getAnggota() {
-        return anggota;
-    }
+public class TugasKelompok {
+//    String name;
+//    String desc;
+//    Date deadline;
+//    ArrayList<UserAgentD> anggota;
+//
+//    TugasKelompok(String name,String desc, Date deadline, UserAgentD user){
+//        this.name = name;
+//        this.desc = desc;
+//        this.deadline = deadline;
+//        this.anggota = new ArrayList<>();
+//        anggota.add(user);
+//    }
+//
+//    public void addAnggota(UserAgentD anggota){
+//        if(!isAnggotaExist(anggota)) {
+//            this.anggota.add(anggota);
+//            anggota.addTugasKelompok(this);
+//        }
+//    }
+//
+//    public void removeAnggota(UserAgentD anggota){
+//        if(isAnggotaExist(anggota)){
+//            this.anggota.remove(anggota);
+//            anggota.removeTugasKelompok(this);
+//        }
+//    }
+//
+//    public boolean isAnggotaExist(UserAgentD anggota){
+//        return this.anggota.contains(anggota);
+//    }
+//
+//    public void remindDeadline(){
+//        for(UserAgentD ang : anggota){
+//            ang.remindTugasKelompok(this);
+//        }
+//    }
+//
+//    @Override
+//    public String getName() {
+//        return name;
+//    }
+//
+//    @Override
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    @Override
+//    public String getDesc() {
+//        return desc;
+//    }
+//
+//    @Override
+//    public void setDesc(String desc) {
+//        this.desc = desc;
+//    }
+//
+//    @Override
+//    public Date getDeadline() {
+//        return deadline;
+//    }
+//
+//    @Override
+//    public void setDeadline(Date deadline) {
+//        this.deadline = deadline;
+//    }
+//
+//    public ArrayList<UserAgentD> getAnggota() {
+//        return anggota;
+//    }
 }
---
+++ src/main/java/com/bot/agentd/repository/TugasIndividuRepository.java
@@ -0,0 +1,23 @@
+package com.bot.agentd.repository;
+
+import com.bot.agentd.core.TugasIndividu;
+
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TugasIndividuRepository extends JpaRepository<TugasIndividu, Long> {
+    @Query(value = "SELECT s FROM TugasIndividu s WHERE s.ownerId = :ownerId")
+    List<TugasIndividu> fetchTugasIndividu(@Param("ownerId") String ownerId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM TugasIndividu s WHERE s.id = :id")
+    void deleteTugasIndividy(@Param("id") String id);
+
+}
